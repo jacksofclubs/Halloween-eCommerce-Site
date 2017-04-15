27c7f866bbce4eaea270e9b43bfa667123f7a258
Kevin Jacks <kevinjackswi@gmail.com>
2017-04-15 09:09:40 -0500

    Fixed issues with cart actions failing to validate.
    Pulling in some of instructor's code caused issues with my version.

--- Acme1/Controllers/ShoppingController.cs
+++ Acme1/Controllers/ShoppingController.cs
@@ -111,22 +111,26 @@ namespace Acme1.Controllers
         {
             try
             {
+                // Validate
                 if (!Valid_CartData(fc)) throw new Exception("Cart data invalid");
+
                 int intresult = 0;
-                int rowid = Convert.ToInt32(fc["rowid"].ToString());
-                string strprodid = fc["prod:" + rowid].ToString();
-                int cartid = 100;
-                //int cartid = 0;
+                // string cartid = 0;
+                // int rowid = Convert.ToInt32(fc["rowid"].ToString());
+                // string strprodid = fc["prod:" + rowid].ToString();
+                // int cartid = 100;
+
+                string straction = fc["action"].ToString();
+                string cartid = fc["cartid"].ToString();
+                string prodid = fc["prodid"].ToString();
+                string strqty = fc["qty"].ToString();
+
                 if (Session["cartnumber"] != null)
-                    cartid = Convert.ToInt32(Session["cartnumber"].ToString());
+                    cartid = Session["cartnumber"].ToString();
+
                 dbcon = GetConnection();
                 dbcon.Open();
-                if (fc["action"] == "update")
-                {
-                    int qty = Convert.ToInt32(fc["qty:" + rowid].ToString());
-                    intresult = CartLineItem.CUDCart(dbcon, "update", cartid, strprodid, qty);
-                } else
-                    intresult = CartLineItem.CUDCart(dbcon, "delete", cartid, strprodid, 0);
+                CartLineItem.CUDCart(dbcon, straction, Int32.Parse(cartid), prodid, Int32.Parse(strqty));
                 dbcon.Close();
                 return RedirectToAction("Cart");
             } catch (Exception ex)
@@ -136,13 +140,12 @@ namespace Acme1.Controllers
         }
         public Boolean Valid_CartData(FormCollection fc)
         {
-            if (fc["rowid"] == null || fc["action"] == null) return false;
-            if (Regex.IsMatch(fc["rowid"].ToString(), @"^([1-9]|[1-9][0-9])$"))
+            if (fc["prodid"] == null || fc["action"] == null) return false;
+            if (Regex.IsMatch(fc["prodid"].ToString(), @"^[A-Za-z0-9]{2,10}$"))
             {
-                int rowid = Convert.ToInt32(fc["rowid"].ToString());
-                if (fc["prod:" + rowid] == null) return false;
-                if (fc["action"] == "update" &&
-                Regex.IsMatch(fc["qty:" + rowid].ToString(), @"^([1-9]|[1-9][0-9])$"))
+                // int rowid = Convert.ToInt32(fc["rowid"].ToString());
+                //if (fc["prod:" + rowid] == null) return false;
+                if (fc["action"] == "update" && Regex.IsMatch(fc["qty"].ToString(), @"^([1-9]|[1-9][0-9])$"))
                     return true;
                 else if (fc["action"] == "delete")
                     return true;
